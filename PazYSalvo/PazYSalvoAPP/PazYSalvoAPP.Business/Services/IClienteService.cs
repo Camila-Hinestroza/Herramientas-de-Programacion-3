f80c68aa9e9e234e48bd5a0d891819c95c6f0159
Camila-Hinestroza <161538126+Camila-Hinestroza@users.noreply.github.com>
2024-04-26 19:30:50 -0500

    Add files via upload

---
+++ PazYSalvo/PazYSalvoAPP/PazYSalvoAPP.Business/Services/IClienteService.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using PazYSalvoAPP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PazYSalvoAPP.Business.Services
+{
+    public interface IClienteService
+    {
+        Task<bool> Insertar(Cliente model);
+        Task<bool> Actualizar(Cliente model);
+        Task<Cliente> Leer(int id); // ?
+        Task<IQueryable<Cliente>> LeerTodos(); // ?
+        Task<bool> Eliminar(int id);
+    }
+}
