35b2c0c3dcf74e1617dfb5557e97dce9c77404e7
Camila-Hinestroza <161538126+Camila-Hinestroza@users.noreply.github.com>
2024-05-24 13:57:41 -0500

    Add files via upload

---
+++ Breakout Game AyC/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace Breakout_Game_AyC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Modern No. 20", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(12, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(78, 21);
+            this.txtScore.TabIndex = 0;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(700, 492);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(100, 32);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ball.Location = new System.Drawing.Point(401, 273);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(25, 25);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.mainGameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(798, 537);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.txtScore);
+            this.Name = "Form1";
+            this.Text = "Breakout Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
