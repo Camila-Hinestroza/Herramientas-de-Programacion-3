5d4bfc49625b7a641e88b56a98a64351eb174be9
Camila-Hinestroza <161538126+Camila-Hinestroza@users.noreply.github.com>
2024-05-28 23:05:48 -0500

    Add files via upload

---
+++ Trabajo_Angular/PazzYSalvoAPP/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using PazzYSalvoApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<FacturaService>(); // Inyectar el servicio
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Inyectar con  Cadena de conexi�n
+builder.Services.AddDbContext<PazzYSalvoApp.Context.PazSalvoApiContext>(c =>
+{
+    c.UseSqlServer(builder.Configuration.GetConnectionString("connString"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PazzYSalvoApp API V1");
+        c.RoutePrefix = string.Empty; 
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
